3e9c2e90963d2221ef77f39b573882dc0f1545f0
ANKUSH PATIL <patil.ankush28@gmail.com>
2023-05-10 12:58:34 +0530

    Added event handler for add cart quantity

--- src/Cart.js
+++ src/Cart.js
@@ -11,59 +11,51 @@ class Cart extends React.Component {
                     price: 9999,
                     title: "Smart Phone",
                     qty: 2,
-                    img: ''
+                    img: "",
                 },
                 {
                     id: 2,
                     price: 761,
                     title: "Watch",
                     qty: 4,
-                    img: ''
+                    img: "",
                 },
                 {
                     id: 3,
                     price: 1238,
                     title: "Hai Dryer",
                     qty: 7,
-                    img: ''
-                }
-            ]
+                    img: "",
+                },
+            ],
         };
     }
 
-
-    increaseQuantity = () => {
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty + 1
-            }
-        }, () => {
-            console.log(this.state.qty);
-        })
+    handleIncreaseQuantity = (product) => {
+        const { products } = this.state;
+        const index = products.indexOf(product);
+        products[index].qty += 1;
+        this.setState({
+            products,
+        });
     };
-
-    decreaseQuantity = () => {
-        if (this.state.qty == 0) {
-            return;
-        }
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty - 1
-            }
-        })
-    }
     render() {
         const { products } = this.state;
         const { title, price, qty } = this.state;
         const arr = [1, 2, 3, 4, 5];
         return (
             <div className="cart">
-                {
-                    products.map((product) => {
-                        return <CartItem product={product} key={product.id} />
-                    })}
+                {products.map((product) => {
+                    return (
+                        <CartItem
+                            product={product}
+                            key={product.id}
+                            onIncreaseQuantity={this.handleIncreaseQuantity}
+                        />
+                    );
+                })}
             </div>
-        )
+        );
     }
 }
 export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -3,26 +3,27 @@ import React from "react";
 
 class CartItem extends React.Component {
 
-    increaseQuantity = () => {
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty + 1
-            }
-        }, () => {
+    // increaseQuantity = () => {
+    //     this.setState((prevState) => {
+    //         return {
+    //             qty: prevState.qty + 1
+    //         }
+    //     }, () => {
 
-        })
-    };
+    //     })
+    // };
 
-    decreaseQuantity = () => {
-        if (this.state.qty == 0) {
-            return;
-        }
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty - 1
-            }
-        })
-    }
+    // decreaseQuantity = () => {
+    //     if (this.state.qty == 0) {
+    //         return;
+    //     }
+    //     this.setState((prevState) => {
+    //         return {
+    //             qty: prevState.qty - 1
+    //         }
+    //     })
+    // }
+
     render() {
         const { title, price, qty } = this.props.product;
         return (
@@ -35,9 +36,13 @@ class CartItem extends React.Component {
                     <div style={{ color: "#777" }}>Rs. {price}</div>
                     <div style={{ color: "#777" }}>Quantity : {qty}</div>
                     <div className="cart-item-actions">
-                        <img alt="increase" className="action-icons" src="https://img.icons8.com/?size=512&id=24717&format=png" onClick={this.increaseQuantity} />
-                        <img alt="decrease" className="action-icons" src="https://img.icons8.com/?size=512&id=1504&format=png" onClick={this.decreaseQuantity} />
-                        <img alt="delete" className="action-icons" src="https://img.icons8.com/?size=512&id=67884&format=png" />
+                        <img alt="increase" className="action-icons"
+                            src="https://img.icons8.com/?size=512&id=24717&format=png"
+                            onClick={() => { this.props.onIncreaseQuantity(this.props.product) }} />
+                        <img alt="decrease" className="action-icons"
+                            src="https://img.icons8.com/?size=512&id=1504&format=png" onClick={this.decreaseQuantity} />
+                        <img alt="delete" className="action-icons"
+                            src="https://img.icons8.com/?size=512&id=67884&format=png" />
                     </div>
                 </div>
             </div>
