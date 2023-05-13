a4349184b9e18d26a618f2db18c05448fda47c5f
ANKUSH PATIL <patil.ankush28@gmail.com>
2023-05-13 12:05:59 +0530

    Added delete feature

--- src/Cart.js
+++ src/Cart.js
@@ -11,21 +11,21 @@ class Cart extends React.Component {
                     price: 9999,
                     title: "Smart Phone",
                     qty: 2,
-                    img: "",
+                    imgSourceURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJg7S_Q0rp7iKHtZA5UQOllLEAtQximiKJHg&usqp=CAU",
                 },
                 {
                     id: 2,
                     price: 761,
                     title: "Watch",
                     qty: 4,
-                    img: "",
+                    imgSourceURL: "https://5.imimg.com/data5/SELLER/Default/2020/12/KN/WP/OI/5388819/t500-smartwatch-250x250.jpg",
                 },
                 {
                     id: 3,
                     price: 1238,
                     title: "Hai Dryer",
                     qty: 7,
-                    img: "",
+                    imgSourceURL: "https://5.imimg.com/data5/VU/UH/MY-3144743/hilips-hp-8234-hairdryer-500x500.jpg",
                 },
             ],
         };
@@ -51,6 +51,14 @@ class Cart extends React.Component {
         });
     }
 
+    onDeleteProduct = (id) => {
+        const { products } = this.state;
+        const items=products.filter((item)=>item.id!==id);
+        this.setState({
+            products:items
+        });
+    };
+
     render() {
         const { products } = this.state;
         return (
@@ -58,7 +66,7 @@ class Cart extends React.Component {
                 {
                     products.map((product) => {
                         return <CartItem product={product} onQtyIncreased={this.onIncrease}
-                            onQtyDecreased={this.onDecrease} />
+                            onQtyDecreased={this.onDecrease} onProductDelete={this.onDeleteProduct}/>
                     })
                 }
             </div>
--- src/CartItem.js
+++ src/CartItem.js
@@ -5,10 +5,11 @@ class CartItem extends React.Component {
 
     render() {
         const { product } = this.props;
+        const{onQtyIncreased,onQtyDecreased,onProductDelete}=this.props;
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image} />
+                    <img style={styles.image} src={product.imgSourceURL}/>
                 </div>
                 <div className="right-block">
                     <div style={{ fontSize: 20 }}>{product.title}</div>
@@ -16,12 +17,12 @@ class CartItem extends React.Component {
                     <div style={{ color: "#777" }}>Quantity :  {product.qty}</div>
                     <div className="cart-item-actions">
                         <img alt="increase" className="action-icons"
-                            src="https://img.icons8.com/?size=512&id=24717&format=png" onClick={() => { this.props.onQtyIncreased(this.props.product) }}
+                            src="https://img.icons8.com/?size=512&id=24717&format=png" onClick={() => { onQtyIncreased(product) }}
                         />
                         <img alt="decrease" className="action-icons"
-                            src="https://img.icons8.com/?size=512&id=1504&format=png" onClick={() => { this.props.onQtyDecreased(this.props.product) }} />
+                            src="https://img.icons8.com/?size=512&id=1504&format=png" onClick={() => { onQtyDecreased(product) }} />
                         <img alt="delete" className="action-icons"
-                            src="https://img.icons8.com/?size=512&id=67884&format=png" />
+                            src="https://img.icons8.com/?size=512&id=67884&format=png"  onClick={() => { onProductDelete(product.id) }}/>
                     </div>
                 </div>
             </div>
